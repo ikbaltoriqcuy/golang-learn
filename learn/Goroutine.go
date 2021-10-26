853a689471449f4113dd789ec5f590c25376d7c8
ikbaltoriq <ikbalok01@gmail.com>
2021-10-26 23:23:52 +0700

    add gocoroutine

--- learn/Goroutine.go
+++ learn/Goroutine.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+
+	f("direct")
+
+	go f("goroutine")
+
+	go f("ikbal")
+
+	go f("toriq")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("done")
+}
